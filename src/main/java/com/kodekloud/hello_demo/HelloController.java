6de367485d006f0c9f864f6fb04d688059f68c5f
(Abdulmunim)Labiyb M Said <88576577+ALabiyb@users.noreply.github.com>
2024-11-12 10:39:12 +0300

    Update HelloController.java

--- src/main/java/com/kodekloud/hello_demo/HelloController.java
+++ src/main/java/com/kodekloud/hello_demo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/hello")
     String hello() {
-        return "Hola, DevOps community!";
+        return "Hello, KodeKloud community!";
     }
 
 }
